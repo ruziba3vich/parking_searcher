a53bcbe58b94d28604f83a93626802bed8775941
ruziba3vich <rruzibayevich@gmail.com>
2025-02-16 19:17:53 +0500

    feat parking_searcher: migrations added

---
+++ migrations/2025-02-16-141249_create_parks/up.sql
@@ -0,0 +1,18 @@
+-- Your SQL goes here
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE parks (
+    park_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    park_name VARCHAR(64) NOT NULL,
+    address VARCHAR(166) NOT NULL,
+    price_ph DOUBLE PRECISION NOT NULL,
+    status VARCHAR(20) NOT NULL CHECK (status IN ('available', 'full', 'closed')),
+    available_spots_count INT NOT NULL,
+    total_spots_count INT NOT NULL,
+    electro_charging_available BOOLEAN NOT NULL DEFAULT FALSE,
+    rating DOUBLE PRECISION DEFAULT 0,
+    park_balance DOUBLE PRECISION DEFAULT 0,
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL
+);
